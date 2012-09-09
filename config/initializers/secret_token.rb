e915dcbb64d20e2a0c8c07fc425e7633db37b77e
RussReynolds <russ@caterdeck.com>
2012-09-08 22:38:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cb5d6c8101531e21aa2f6cb6bebd207a99e6aeb0a7b8383370f409387ed0fcf48b7fde163afd758b7764acdea13ca178dfc84d5fe00430d183329526b50d1095'
